77209152710a56914a97a2354b4503b9831e26dc
Ilya Marchenko <newilia2@gmail.com>
2023-10-01 01:35:50 +0200

    AiPlatformController aggressive behavior WIP

--- src/Pong/AiPlatformController.cpp
+++ src/Pong/AiPlatformController.cpp
@@ -3,16 +3,47 @@
 #include "PongPlatform.h"
 
 AiPlatformController::AiPlatformController(PongPlatform* platform) : UserPlatformController(platform) {
+}
 
+void AiPlatformController::beginObserve(const std::weak_ptr<PongPlatform>& opponentPlatform,
+	const std::weak_ptr<PongBall>& ball)
+{
+	mOpponentPlatform = opponentPlatform;
+	mBall = ball;
+}
+
+void AiPlatformController::setAggressivenessParams(float aggression, const sf::Time& changePeriod) {
+	mAggression = aggression;
+	mAggressionChangePeriod = changePeriod;
 }
 
 void AiPlatformController::observeState() {
 	if (auto ball = mBall.lock()) {
-		ObservedState state = {
+		ExternalState state = {
 			.ballPos = ball->getShape()->getPosition()
 		};
-		mObservedStates.push(state);
+		mExternalStateTimers.push({ sf::Clock(), state });
 	}
+}
+
+void AiPlatformController::react() {
+	movePlatformTowardsBall();
+}
+
+void AiPlatformController::movePlatformTowardsBall() {
+	float distanceToBall = 0.f;
+	if (auto ball = mBall.lock()) {
+		distanceToBall = abs(mCurExState.ballPos.y - mPlatform->getShape()->getPosition().y) - ball->getShape()->getRadius() - mPlatform->getBbox().height;
+	}
+
+	float steadyRatio = 0.f;
+	if (auto opponentPlatform = mOpponentPlatform.lock()) {
+		float distanceBetweenPlatforms = abs(mPlatform->getShape()->getPosition().y - opponentPlatform->getShape()->getPosition().y);
+		//this value affects on how much AI will move platform to center as the ball is far away from him
+		steadyRatio = 0.2f * std::clamp(distanceToBall / distanceBetweenPlatforms, 0.f, 1.f);
+	}
+
+	mTargetPos.x = mCurExState.ballPos.x * (1.f - steadyRatio) + mDefaultPos.x * steadyRatio;
 }
 
 void AiPlatformController::update(const sf::Time& dt) {
@@ -21,22 +52,24 @@ void AiPlatformController::update(const sf::Time& dt) {
 		observeState();
 	}
 
-	if (!mObservedStates.empty()) {
-		auto& state = mObservedStates.front();
-		auto waitingTime = state.waitingTime.getElapsedTime();
-		if (waitingTime > mReactionDelay) {
-			auto ball = mBall.lock();
-			auto opponentPlatform = mOpponentPlatform.lock();
-			if (ball && opponentPlatform) {
-				float distanceToBall = abs(state.ballPos.y - mPlatform->getShape()->getPosition().y) - ball->getShape()->getRadius() - mPlatform->getBbox().height;
-				float distanceBetweenPlatforms = abs(mPlatform->getShape()->getPosition().y - opponentPlatform->getShape()->getPosition().y);
-				float steadyRatio = 0.2f * std::clamp(distanceToBall / distanceBetweenPlatforms, 0.f, 1.f);
+	if (mAggressionChangeTimer.getElapsedTime() > mAggressionChangePeriod) {
+		mAggressionChangeTimer.restart();
+		constexpr float aggressionVariability = 0.5f;
+		mAggression = mAggression * (1.f - aggressionVariability) + 
+			static_cast<float>(std::rand() % 256) / 255.f * aggressionVariability;
+	}
 
-				mTargetPos.x = state.ballPos.x * (1.f - steadyRatio) + mDefaultPos.x * steadyRatio;
-			}
-
-			mObservedStates.pop();
+	if (!mExternalStateTimers.empty()) {
+		auto& stateTimer = mExternalStateTimers.front();
+		;
+		if (auto waitingTime = stateTimer.timePassed.getElapsedTime();  waitingTime > mReactionDelay) {
+			mPrevExState = mCurExState;
+			mCurExState = stateTimer.state;
+			mExternalStateTimers.pop();
+			react();
 		}
 	}
+
+
 	UserPlatformController::update(dt);
 }
--- src/Pong/AiPlatformController.h
+++ src/Pong/AiPlatformController.h
@@ -10,28 +10,41 @@
 class AiPlatformController : public UserPlatformController {
 public:
 	explicit AiPlatformController(PongPlatform* platform);
-	void beginObserve(std::weak_ptr<PongPlatform> opponentPlatform, std::weak_ptr<PongBall> ball);
 	void update(const sf::Time& dt) override;
 	void setReactionDelay(const sf::Time& delay) { mReactionDelay = delay; }
 	void setObservePeriod(const sf::Time& period) { mObservePeriod = period; }
+	void beginObserve(const std::weak_ptr<PongPlatform>& opponentPlatform, const std::weak_ptr<PongBall>& ball);
+	void setAggressivenessParams(float aggression, const sf::Time& changePeriod);
 
 private:
-	void observeState();
-
-	struct ObservedState {
-		sf::Clock waitingTime;
+	struct ExternalState {
 		sf::Vector2f ballPos;
 	};
+
+	struct ExternalStateTimer {
+		sf::Clock timePassed;
+		ExternalState state;
+	};
+
+	struct InternalState {
+		float activity = 0.1f;
+	};
+
+	void observeState();
+	void react();
+
+	// reactions
+	void movePlatformTowardsBall();
 
 	std::weak_ptr<PongPlatform> mOpponentPlatform;
 	std::weak_ptr<PongBall> mBall;
 	sf::Time mReactionDelay;
 	sf::Time mObservePeriod;
 	sf::Clock mObserveTimer;
-	std::queue<ObservedState> mObservedStates;
+	std::queue<ExternalStateTimer> mExternalStateTimers;
+	ExternalState mCurExState;
+	ExternalState mPrevExState;
+	float mAggression = 0.5f;
+	sf::Time mAggressionChangePeriod;
+	sf::Clock mAggressionChangeTimer;
 };
-
-inline void AiPlatformController::beginObserve(std::weak_ptr<PongPlatform> opponentPlatform, std::weak_ptr<PongBall> ball) {
-	mOpponentPlatform = opponentPlatform;
-	mBall = ball;
-}
