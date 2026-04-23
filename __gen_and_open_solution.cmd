d73ed957d864b1696b0dda60b7a3f1102b653cee
Ilya Marchenko <@>
2026-04-24 00:12:58 +0200

    project tweaks and cleanup

--- __gen_and_open_solution.cmd
+++ __gen_and_open_solution.cmd
@@ -9,5 +9,7 @@ if not exist "build" mkdir "build"
 cmake -S . -B build -A x64
 if errorlevel 1 exit /b %errorlevel%
 
+if exist "build\TestEngine.slnx" start "" "%~dp0build\TestEngine.slnx"
+
 exit /b 0
 
