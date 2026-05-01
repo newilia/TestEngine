d62be5732d5b0281e2aab3b7870f1da42823fbdf
Ilya Marchenko <@>
2026-05-01 04:32:48 +0200

    CollisionBehaviour meta class

--- src/Engine/Behaviour/Physics/CollisionBehaviour.h
+++ src/Engine/Behaviour/Physics/CollisionBehaviour.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include "Engine/Behaviour/Behaviour.h"
+#include "Engine/Core/MetaClass.h"
 #include "Engine/Core/Signal.h"
 
 #include <bitset>
 
 struct IntersectionDetails;
 
+// TODO move to base collider
 class CollisionBehaviour : public Behaviour
 {
+	META_CLASS()
 public:
-	std::bitset<8> _collisionGroups;
+	static constexpr int CollisionGroupsCount = 8;
+	// TODO support @property for bitset with custom editor widget (checkboxes for each bit)
+	std::bitset<CollisionGroupsCount> _collisionGroups;
 	Signal<const IntersectionDetails&> _collisionCallbacks;
 };
