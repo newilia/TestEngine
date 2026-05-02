fbf68e3effbe56ccb1c763646ae9b66b5c97bb25
Ilya Marchenko <@>
2026-05-02 04:27:14 +0200

    add: Verify() macro, cmake: reduce RelWithDebugInfo optimizations, add global header

---
+++ src/TestEngineGlobals.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cassert>
+
+#define Verify(...)                                                                                                    \
+	([&] {                                                                                                             \
+		auto _teVerifyV = (__VA_ARGS__);                                                                               \
+		assert(static_cast<bool>(_teVerifyV));                                                                         \
+		return _teVerifyV;                                                                                             \
+	}())
