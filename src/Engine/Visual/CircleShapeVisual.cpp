19f1daf058f8414ed1ea593c115cc2538ad16886
Ilya Marchenko <newilia2@gmail.com>
2026-05-13 22:48:13 +0200

    add circle sector draw

--- src/Engine/Visual/CircleShapeVisual.cpp
+++ src/Engine/Visual/CircleShapeVisual.cpp
@@ -1,6 +1,7 @@
 #include "Engine/Visual/CircleShapeVisual.h"
 
 #include "CircleShapeVisual.generated.hpp"
+#include "Engine/Core/Utils.h"
 
 #include <algorithm>
 #include <cstddef>
@@ -26,6 +27,37 @@ float CircleShapeVisual::GetRadius() const {
 void CircleShapeVisual::SetRadius(float radius) {
 	_circle.setRadius(radius);
 	_circle.setOrigin({radius, radius});
+	_isSectorDirty = true;
+}
+
+void CircleShapeVisual::Draw(sf::RenderTarget& target, sf::RenderStates states) const {
+	target.draw(_circle, states);
+
+	if (_drawSector) {
+		if (_isSectorDirty) {
+			RebuildSectorVertices();
+		}
+		sf::RenderStates sectorStates = states;
+		sectorStates.transform.translate(_circle.getPosition());
+		target.draw(_sectorVertices, sectorStates);
+	}
+}
+
+void CircleShapeVisual::RebuildSectorVertices() const {
+	constexpr int kSectorPoints = 10;
+	constexpr sf::Angle kAngleStep = sf::degrees(30) / (kSectorPoints - 1);
+	const float radius = GetRadius();
+
+	_sectorVertices.setPrimitiveType(sf::PrimitiveType::TriangleFan);
+	_sectorVertices.resize(kSectorPoints + 2);
+	_sectorVertices[0].position = {0.f, 0.f};
+	_sectorVertices[kSectorPoints + 1].position = {0.f, 0.f};
+
+	for (int i = 0; i < kSectorPoints; i++) {
+		const sf::Angle angle = kAngleStep * i;
+		_sectorVertices[i + 1].position = Utils::Rotate(sf::Vector2f(0, radius), angle.asRadians());
+	}
+	_isSectorDirty = false;
 }
 
 sf::Vector2f CircleShapeVisual::GetOrigin() const {
@@ -43,4 +75,12 @@ int CircleShapeVisual::GetPointCount() const {
 void CircleShapeVisual::SetPointCount(int count) {
 	const int clamped = std::clamp(count, kMinCirclePoints, kMaxCirclePoints);
 	_circle.setPointCount(static_cast<std::size_t>(clamped));
+}
+
+bool CircleShapeVisual::GetDrawSector() const {
+	return _drawSector;
+}
+
+void CircleShapeVisual::SetDrawSector(bool drawSector) {
+	_drawSector = drawSector;
 }
--- src/Engine/Visual/CircleShapeVisual.h
+++ src/Engine/Visual/CircleShapeVisual.h
@@ -4,6 +4,7 @@
 #include "Engine/Visual/ShapeVisualBase.h"
 
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
 
 class CircleShapeVisual : public ShapeVisualBase
 {
@@ -12,6 +13,7 @@ class CircleShapeVisual : public ShapeVisualBase
 
 public:
 	CircleShapeVisual();
+	void Draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 	const sf::Shape* GetBaseShape() const override;
 
 public:
@@ -27,7 +29,16 @@ public:
 	sf::Vector2f GetOrigin() const;
 	/// @setter
 	void SetOrigin(const sf::Vector2f& origin);
+	/// @getter
+	bool GetDrawSector() const;
+	/// @setter
+	void SetDrawSector(bool drawSector);
 
 private:
+	void RebuildSectorVertices() const;
+
+	bool _drawSector = true;
 	sf::CircleShape _circle;
+	mutable sf::VertexArray _sectorVertices;
+	mutable bool _isSectorDirty = true;
 };
