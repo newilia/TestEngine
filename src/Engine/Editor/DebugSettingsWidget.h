f4c5c16103a8eabe94ad36bb3e86db06c21431d9
Ilya Marchenko <newilia2@gmail.com>
2026-05-13 20:07:52 +0200

    DebugSettingsWidget refactor

--- src/Engine/Editor/DebugSettingsWidget.h
+++ src/Engine/Editor/DebugSettingsWidget.h
@@ -1,9 +1,20 @@
 #pragma once
 
+class PhysicsProcessor;
+
 namespace Engine {
+	class MainContext;
+
 	class DebugSettingsWidget
 	{
 	public:
 		void Draw() const;
+
+	private:
+		void DrawSimulationSettings(MainContext& mainContext, PhysicsProcessor* physicsProc) const;
+		void DrawPhysicsSettings(MainContext& mainContext, PhysicsProcessor* physicsProc) const;
+		void DrawVisualizationSettings(MainContext& mainContext) const;
+		void DrawPerformanceInfo(MainContext& mainContext) const;
+		void DrawRenderSettings(MainContext& mainContext) const;
 	};
 } // namespace Engine
